a7853f2c3c8f2a5f27e05ad2839e86425ee5d8ed
IsmailMan81F <ismailman81fmeg@gmail.com>
2026-01-14 21:38:44 +0100

    update the background image

--- src/App.jsx
+++ src/App.jsx
@@ -5,27 +5,47 @@ import BlackCardVisual from "./BlackCardVisual";
 import WhiteVisualCard from "./WhiteVisualCard";
 import Line from "./Line";
 import Lines from "./Lines";
+import BgImage from "./assets/background-image.jpg";
 
 export default function App() {
   return (
-    <div className="app">
-      <Navbar />
-      <Content />
+    <>
       <div
+        className="overlay"
         style={{
-          width: 229,
-          height: 200,
-          backgroundColor: "white",
           position: "absolute",
-          top: 320,
-          right: 400,
+          inset: 0,
+          minHeight: "100vh",
+          background:
+            "linear-gradient(90deg,rgba(255, 255, 255, 0.92) 0%,  white 30%, rgba(255, 255, 255, 0.68))",
+          zIndex: 1,
         }}
       ></div>
-      <BlackCardVisual />
-      <WhiteVisualCard />
-      <Line />
-
-
-    </div>
+      <div
+        className="app"
+        style={{
+          backgroundImage: `url(${BgImage})`,
+          backgroundSize: "cover",
+          position: "relative",
+        }}
+      >
+        <div className="content" style={{ zIndex: 2, position: "relative" }}>
+          <Navbar />
+          <Content />
+          <div
+            style={{
+              width: 229,
+              height: 200,
+              backgroundColor: "white",
+              position: "absolute",
+              top: 320,
+              right: 400,
+            }}
+          ></div>
+          <BlackCardVisual />
+          <WhiteVisualCard />
+        </div>
+      </div>
+    </>
   );
 }
